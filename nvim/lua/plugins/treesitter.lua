623f9e66afe3148a5d6e1bdd5b30dc7c2144cd8f
Samuel Janas <sneakyinsect@gmail.com>
2025-04-29 21:01:58 +0200

    update tmux conf, add nvim-autopairs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function ()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-        ensure_installed = {"c", "lua", "vim", "html", "python", "go", "markdown", "sql", "yaml"},
+        ensure_installed = {"c", "c_sharp", "lua", "vim", "html", "python", "go", "markdown", "sql", "yaml", "dockerfile"},
         auto_install = true,
         sync_install = false,
         highlight = { enable = true },
