1942824fa67453c1a72501ad5beea11544c00853
Samuel Janas <sneakyinsect@gmail.com>
2026-07-13 11:34:10 +0200

    band-aid on telescope searching

--- nvim/lua/other_plugins.lua
+++ nvim/lua/other_plugins.lua
@@ -7,9 +7,22 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function(_, opts)
+      local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+      if ok and not parsers.ft_to_lang then
+        parsers.ft_to_lang = function(ft)
+          return vim.treesitter.language.get_lang(ft) or ft
+        end
+      end
+
+      require("telescope").setup(opts)
+    end,
     opts = {
       defaults = {
         file_ignore_patterns = { ".git", "node_modules", ".venv", "requirements.txt", ".lock" },
+        preview = {
+          treesitter = false,
+        },
       },
     },
   },
